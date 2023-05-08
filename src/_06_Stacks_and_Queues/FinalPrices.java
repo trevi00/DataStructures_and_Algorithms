5d6bbfd98fb79ac207256c2f73a14722f733979b
tnsu0 <gohardks00@gmail.com>
2023-05-08 15:28:40 +0900

    수정

--- src/_06_Stacks_and_Queues/FinalPrices.java
+++ src/_06_Stacks_and_Queues/FinalPrices.java
@@ -1,7 +1,13 @@
 package _06_Stacks_and_Queues;
 
 public class FinalPrices {
-    public int[] finalPrices(int[] prices) { // O(N)으로 만들기
+    public static void main(String[] args) {
+        int[] prices = {8, 4, 6, 2, 3};
+        for (int price: finalPrices(prices)) {
+            System.out.print(price + " ");
+        }
+    }
+    public static int[] finalPrices(int[] prices) { // O(N)으로 만들기
         int[] results = new int[prices.length];
 
         for(int i=0; i<prices.length; i++) {
